de749df5e72948a8dea33cb2e23207d33509cd07
rafael330 <143340698+rafael330@users.noreply.github.com>
2025-04-09 22:47:28 -0300

    Update tela_cadastro_streamlit.py

--- tela_cadastro_streamlit.py
+++ tela_cadastro_streamlit.py
@@ -840,8 +840,8 @@ if 'opcao' not in st.session_state:
     st.session_state.opcao = "Novo Cadastro"
 
 # Menu lateral
-image = "https://github.com/rafael330/srtransporte/blob/main/WhatsApp%20Image%202025-04-09%20at%2021.19.07.jpeg"
-st.sidebar.image(image, width=200)
+image = "https://github.com/rafael330/srtransporte/blob/main/WhatsApp%20Image%202025-04-09%20at%2021.19.07.png"
+st.sidebar.image(image, width=50)
 
 st.sidebar.title("Menu")
 
